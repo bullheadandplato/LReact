68839d85c6db10a831b64b55895dadfbe810c069
bullhead <m.osamabinomar@gmail.com>
2019-09-29 23:55:27 +0500

    basic redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Counter from "./counter/Counter";
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+
+const initialState = {
+    count: 0
+};
+
+const reducer = (state = initialState, action) => {
+    if (action.type === "INC") {
+        return {count: state.count + 1};
+    } else if (action.type === "DEC") {
+        return {count: state.count - 1};
+    }
+    return state;
+};
+
+const store = createStore(reducer);
 
 function App() {
-  return (
-      <div>
-          <Counter/>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <Counter/>
+        </Provider>
+    );
 }
 
 export default App;
--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,18 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class Counter extends React.Component {
-    state = {count: 0};
 
     increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
+        this.props.dispatch({type: "INC"});
     };
 
     decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        });
+        this.props.dispatch({type: "DEC"});
     };
 
     render() {
@@ -21,7 +17,7 @@ class Counter extends React.Component {
                 <h2>Counter</h2>
                 <div>
                     <button onClick={this.decrement}>-</button>
-                    <span>{this.state.count}</span>
+                    <span>{this.props.count}</span>
                     <button onClick={this.increment}>+</button>
                 </div>
             </div>
@@ -29,4 +25,10 @@ class Counter extends React.Component {
     }
 }
 
-export default Counter;
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    }
+}
+
+export default connect(mapStateToProps)(Counter);
